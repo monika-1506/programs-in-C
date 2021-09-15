5c64d19ea8b1f289ff1b7595d060a52080b3d75b
monika-1506 <80668502+monika-1506@users.noreply.github.com>
2021-09-15 13:34:52 -0700

    program2

---
+++ 2program.c
@@ -0,0 +1,22 @@
+
+/*
+2. Write a C program to get the absolute difference between n and 51. If n is greater than 51 return
+triple the absolute difference.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+printf("%d %d%d",smple(33, 88 ,51));
+}
+ int smple(int n)
+        {
+         const int a = 51;
+
+            if (n > a)
+            {
+                return (n - a)*3;
+            }
+            return a - n;
+        }
+}
+  
